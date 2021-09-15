4a5d06f6ce0966b718b0a35bba3526ee16685692
Siddy <dancore99@outlook.com>
2021-09-15 22:56:48 +0800

    i have started from fresh from my last upload. i have created several more pages and css plus 2 js. it is very glitch and believe i could have wokred a little long on this but must do what i must do.

--- assets/style.js
+++ assets/style.js
@@ -1,48 +1,212 @@
 
-var highScoreEl = document.getElementById("#high-scores");
-var startBtn = document.getElementById("#start")
-var timerEl = document.getElementById("#time")
-var  alts = document.querySelectorAll(".posAns");
+var countdownTimeText = document.querySelector(".timer-text");
+var startButton = document.querySelector(".start-button");
+var questionEl = document.getElementById("question");
+var choiceA = document.getElementById("choice-a");
+var choiceB = document.getElementById("choice-b");
+var choiceC = document.getElementById("choice-c");
+var choiceD = document.getElementById("choice-d");
+var choiceContainers = document.querySelector(".choice-container");
+var answerCard = document.getElementById("game");
+var gameEl = document.querySelector(".container");
+var scoreEl = document.getElementById("score");
+var questionCounterEl = document.getElementById("question-counter");
+var gameHud = document.querySelector(".game-info");
+var endGameScore = document.querySelector(".exit-page");
+var finalScoreEl = document.getElementById("final-score");
+var saveScoreBtn = document.getElementById('save-score-btn');
+const username = document.getElementById('username');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 
-var question = [ 
+var currentQuestion = {};
+var availableQuestions = [];
+var score = 0;
+var questionCounter = 0;
+var timer;
+var timeLeft;
+var question;
+var possibleAnswers;
+var finalScore;
+
+var questions = [
     {
-    title: "What is NOT a data type in JavaScipt?",
-    possibleAnswer: ['Number', 'string', 'font-size'],
-    correctAnswer:2
+        question: "what ",
+        answers: ["a. let", "b. const", "c. my variable is"],
+        correctAnswer: 0
     },
-    {
-    title: "What does DOM Stand for?",
-    possibleAnswer: ['Document object model', 'domestic operation motor', 'dumb oriention moduale'],
-    correctAnswer:0
+    {     
+        question: "what is the correct syntax to log something to our console?",
+        answers: ["a. please print this", "b. console.log()", "c. consolelog[]"],
+        correctAnswer: 1
     },
-    {
-    title: "What element is NOT part of javascript?",
-    possibleAnswer:['array', 'id', 'random'],
-    correctAnswer:1
-    }
-];
+    {     
+        question: "how can we change the style of an element on our html page",
+        answers: ["a. js-color-changer", "b. colour-to", "c. css.change"],
+        correctAnswer: 3
+    },
+];  
+const CORRECT_BONUS = 2;
+const MAX_QUESTIONS = 3;
+const TIME_PENALTY = 5;
+const DELAY = 750;
+const MAX_HIGHSCORES = 3;
 
-function showQuestion() {
-   // for (i = 0; i < question.length; i++){
-   //     question[i]
-    //}
-var title = document.getElementById('question-title');
-title.textContent = question.title;
-document.getElementById("#question-title");
-console.log(alts);
-
-alts.forEach(function(element, index) {
-    element.textContent = question.possibleAnswer[index];
-
-    element.addEventListener('click', function() {
-        if (question.correctAnswer == index) {
-           console.log("a")
-
-        } else {
-            console.log("wrong")
-        }
-    })
-});
+function init() {
 }
-showQuestion()
+
+function startGame() {
+    answerCard.style = "display:inline";
+    gameHud.style = "display:flex";
+    timeLeft = 20;
+    questionCounter = 0;
+    score = 0; 
+    availableQuestions = [...questions] 
+    startTimer()
+    loadQuestion();
+    startButton.style = "display:none";
+}
+
+function startTimer() {
+    timer = setInterval(function() {
+        timeLeft --;
+        renderScores();
+        if (timeLeft >=0 ) {
+            if (questionCounter > questions.length  && timeLeft > 0) {
+                clearInterval(timer);
+                gameOver();
+            }
+        }
+        if (timeLeft === 0) { 
+            clearInterval(timer); 
+            gameOver();     
+        }
+    }, 1000);
+}
+
+
+function renderScores() {
+    scoreEl.textContent = score
+    countdownTimeText.textContent = timeLeft;
+}
+
+
+function saveHighscore(event) {
+    event.preventDefault(); 
+
+    const scoreToStore = {
+        score: finalScore,
+        name: username.value
+    };
+
+    highScores.push(scoreToStore); 
+    highScores.sort((a,b) => {
+        return b.score - a.score;
+    } )
+
+    highScores.splice(3);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    clearHighscoreHud(); 
+}
+
+
+function clearHighscoreHud() { 
+    username.style = "display:none"; 
+    saveScoreBtn.style = "display:none";
+    finalScoreEl.textContent = 'saved.';
+}
+
+username.addEventListener('keyup', function() { 
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveScoreBtn.addEventListener('click', saveHighscore);
+
+function gameOver () {
+    gameEl.style = "display:none"; 
+    getFinalScore();
+    gameHud.style = "display:none";
+    endGameScore.style = "display:flex";
+    finalScoreEl.textContent = finalScore; 
+}
+
+function getFinalScore () {
+    var scoreAdd = score * timeLeft 
+    finalScore = scoreAdd
+}
+
+function loadQuestion () {
+    questionCounter++;
+    questionCounterEl.textContent = questionCounter + '/3'; 
+
+   if (questionCounter > questions.length && questionCounter >= MAX_QUESTIONS) { 
+        return gameOver();
+   }
+    
+    var i = Math.floor(Math.random() * availableQuestions.length); 
+    currentQuestion = availableQuestions[i];
+    questionEl.textContent = currentQuestion.question;
+
+    choiceA.textContent = availableQuestions[i].answers[0]; 
+    choiceB.textContent = availableQuestions[i].answers[1];
+    choiceC.textContent = availableQuestions[i].answers[2];
+    
+
+    availableQuestions.splice(i, 1); 
+};
+
+answerCard.addEventListener('click', checkAnswer);
+
+function checkAnswer(event) {
+    const selectedChoice = event.target; 
+    const selectedAnswer = selectedChoice.dataset["number"]; 
+    
+    if (!event.target.classList.contains('choice-text')) return 
+
+    if ( String(selectedAnswer) === String(currentQuestion.correctAnswer)) { 
+        correct();
+        setBackgroundGreen(event.target);
+    } else {
+        incorrect();
+        setBackgroundRed(event.target);
+    }
+    setTimeout(() => {
+        loadQuestion();
+    }, DELAY)
+    renderScores();
+}
+
+function setBackgroundGreen(element) {
+    element.classList.add('background-green'); 
+    setTimeout(() => {
+        element.classList.remove('background-green');
+    }, DELAY)
+}
+
+function setBackgroundRed(element) {
+    element.classList.add('background-red');
+    setTimeout(() => {
+        element.classList.remove('background-red');
+    }, DELAY)
+}
+
+function correct() {
+    score += CORRECT_BONUS; 
+    scoreEl.classList.add('background-green'); 
+    setTimeout(() => {
+        scoreEl.classList.remove('background-green');
+    }, DELAY)
+}
+
+function incorrect() {
+    timeLeft -= TIME_PENALTY;
+    countdownTimeText.classList.add('background-red');
+    setTimeout(() => {
+        countdownTimeText.classList.remove('background-red');
+    }, DELAY)
+}
+
+startButton.addEventListener("click", startGame); 
+
+init();
